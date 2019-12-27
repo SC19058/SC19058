919bd15becfa559d84efbba19db5dab8982b0dd4
qiyuan98 <447283629@qq.com>
2019-12-27 17:29:30 +0800

    update
    update functions and homework is included in vignettes

--- R/d_stat.R
+++ R/d_stat.R
@@ -6,6 +6,18 @@
 #' @param index for permutation test, indicate how to divide the samples into groups
 #' @param distance the distance matrix
 #' @return energy statistics used for multivariate test for equal distribution
+#' @examples
+#' \dontrun{ 
+#' pdim <- 10;delta <- 0.5;size = 30;group = 4
+#' dat <- c(dat <- c(rt(n = size*pdim,df = 4,ncp = delta),
+#'                   rt(n = size*pdim*(group - 1),df = 4))
+#' dat <- matrix(dat,byrow = TRUE,nrow = total.size)
+#' distance <- as.matrix(dist(dat,diag = TRUE,upper = TRUE,
+#'                            method = "minkowski",p = 1))
+#' index <- 1:size*group
+#' d_stat(size,group,index,distance)
+#' }
+#' @export
 d_stat <- function(size,group,index,distance){
   total <- size * group
   ind <- index;ind <- matrix(ind,byrow = TRUE,nrow = group)
